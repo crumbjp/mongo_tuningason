c0e549a5f29e2e7728cdc332885acf88078cd3ac
crumbjp <hiroaki.kubota@mail.crumb.jp>
2016-06-02 15:22:37 +0900

    Fix forever

--- panel/forever.sh
+++ panel/forever.sh
@@ -3,5 +3,6 @@ DIR=`dirname $0`
 cd ${DIR}
 DIR=`pwd`
 export NODE_PATH=.
+${DIR}/node_modules/forever/bin/forever stopall
 ${DIR}/node_modules/forever/bin/forever start -l ${DIR}/log/server.log --spinSleepTime 1000 -a ./bin/www
 ${DIR}/node_modules/forever/bin/forever start -l ${DIR}/log/loader.log --spinSleepTime 1000 -a ./bin/loader.js
